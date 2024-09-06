6e4d6954a768e51b6174fbc4a75c2701a89aba56
hoodmanb <joshuadebravo@gmail.com>
2024-09-06 22:58:34 +0100

    update

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -22,14 +22,14 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error):
+def unauthorized(error) -> str:
     """request unauthorized
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error):
+def forbidden(error) -> str:
     """request Forbidden
     """
     return jsonify({"error": "Forbidden"}), 403
